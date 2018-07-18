36d5dd11d4093541837eb9668f03b9dc33b99d1b
Ephraim Becker <ephraimbecker@Ephriams-iMac.local>
2018-07-18 13:06:02 -0400

    The home nav-link no longer shows active state on the add event page.

--- addEvent.php
+++ addEvent.php
@@ -52,7 +52,7 @@ $result = mysqli_query($link, $query);
 
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
           <ul class="navbar-nav mr-auto">
-            <li class="nav-item active">
+            <li class="nav-item">
               <a class="nav-link" href="index.php">Home <span class="sr-only">(current)</span></a>
             </li>
           </ul>
