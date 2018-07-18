3e15c90c13bbc0074a36d63711a4b4a5aa898d0a
Ephraim Becker <ephraimbecker@Ephriams-iMac.local>
2018-07-18 13:08:19 -0400

    Removed screen reader currect for home nav-link on the add event page.

--- addEvent.php
+++ addEvent.php
@@ -53,7 +53,7 @@ $result = mysqli_query($link, $query);
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
           <ul class="navbar-nav mr-auto">
             <li class="nav-item">
-              <a class="nav-link" href="index.php">Home <span class="sr-only">(current)</span></a>
+              <a class="nav-link" href="index.php">Home</a>
             </li>
           </ul>
           <ul class="navbar-nav mr-right">
