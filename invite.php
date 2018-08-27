8ca418e40c971d97e2478655bd2c192b1efe7377
Ephraim Becker <emb16@outlook.com>
2018-08-27 13:13:03 -0400

    Trying to fix bug where it allows you to invite a user that's already invited.

--- invite.php
+++ invite.php
@@ -34,6 +34,9 @@ $allUsersEmailResult = mysqli_query($link, $allUsersEmailQuery);
 $user_id_query = "SELECT * FROM users WHERE username='$session_username'";
 $user_id_result = mysqli_query($link, $user_id_query);
 
+$user_id_invited_query = "SELECT * FROM users LEFT OUTER JOIN invite ON users.user_id=invite.user_id WHERE invite.event_id='$event_id'";
+$user_id_invited_result = mysqli_query($link, $user_id_invited_query);
+
 $_SESSION['user_id'] = mysqli_fetch_array($user_id_result)[0];
 $session_user_id = $_SESSION['user_id'];
 
@@ -227,13 +230,10 @@ $darkTheme = mysqli_fetch_array($darkTheme_result)[0];
         <br>
         <br>
           <?php
-					$invited_query = "SELECT * FROM invite LEFT OUTER JOIN users ON invite.user_id=users.user_id WHERE event_id='$event_id'";
-					$invited_result = mysqli_query($link, $invited_query);
-
-          while($user = mysqli_fetch_array($allUsers_result)[0]){
+					while($user = mysqli_fetch_array($allUsers_result)[0]){
             $allUsersEmail = mysqli_fetch_array($allUsersEmailResult)[0];
 
-            if($user != $session_username && $allUsersEmail != $email && $user != mysqli_fetch_array($invited_result)['username']){
+            if($user != $session_username && $allUsersEmail != $email && $user != mysqli_fetch_array($user_id_invited_result)['username']){
               $allUsersEmail_hash = md5(strtolower(trim($allUsersEmail)));
 
 							if($darkTheme == 0){
