f9a3ad97afd54618917a8b199319b199f5254f9e
Ephraim Becker <emb16@outlook.com>
2018-10-17 15:41:21 -0400

    Layout improvements.

--- findFriends.php
+++ findFriends.php
@@ -226,7 +226,7 @@ $findFriends_result = mysqli_query($link, $findFriends_query);
             <div class="row">
               <img class="align-middle col-auto" src="https://www.gravatar.com/avatar/<?php echo $user_email_hash ?>?s=150" width="50" height="50">
               &ensp;<p class="col"><?php echo $username ?></p>
-              <a href="#" class="btn btn-primary col">Add friend <i class="material-icons align-text-top">person_add</i></a>
+              <a href="#" class="btn btn-primary col-2 mr-5">Add friend <i class="material-icons align-text-top">person_add</i></a>
             </div>
           </li>
           <?php
