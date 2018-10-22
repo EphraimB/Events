b492589201e3f529f213cc9de642d1ab32a58180
Ephraim Becker <emb16@outlook.com>
2018-10-22 13:45:32 -0400

    Content is now centered in the index page.

--- index.php
+++ index.php
@@ -227,9 +227,9 @@ $darkTheme = mysqli_fetch_array($darkTheme_result)[0];
         ?>
         <br>
 				<br>
-        <div class="row font-weight-bold mb-4">
+        <div class="row justify-content-around font-weight-bold mb-4">
           <div class="col-4 col-lg">Title</div>
-          <div class="col-5 col-lg">Description</div>
+          <div class="col-4 col-lg">Description</div>
           <div class="col-lg-2 d-none d-lg-block">Location</div>
           <div class="col-lg d-none d-lg-block">Start date</div>
           <div class="col-lg d-none d-lg-block">End date</div>
@@ -261,9 +261,9 @@ $darkTheme = mysqli_fetch_array($darkTheme_result)[0];
 
           ?>
 
-        <div class="row mb-4">
+        <div class="row justify-content-around mb-4">
           <div class="col-4 col-lg"><a href="moreInfo.php?event_id=<?php echo $upcomingEvent_id ?>&userEvents_id=<?php echo $upcomingUserEvents_id ?>&invitedEvent=false"><?php echo $upcomingTitle ?></a></div>
-          <div class="col-5 col-lg"><?php echo $upcomingDescription ?></div>
+          <div class="col-4 col-lg"><?php echo $upcomingDescription ?></div>
           <div class="col-lg-2 d-none d-lg-block"><?php echo $upcomingLocation ?></div>
           <div class="col-lg d-none d-lg-block"><?php echo $upcomingStartDateFormatted."<br>".$upcomingStartTimeFormatted ?></div>
           <div class="col-lg d-none d-lg-block"><?php echo $upcomingEndDateFormatted."<br>".$upcomingEndTimeFormatted ?></div>
@@ -291,9 +291,9 @@ $darkTheme = mysqli_fetch_array($darkTheme_result)[0];
         ?>
         <br>
 				<br>
-        <div class="row font-weight-bold mb-4">
+        <div class="row justify-content-around font-weight-bold mb-4">
           <div class="col-4 col-lg">Title</div>
-          <div class="col-5 col-lg">Description</div>
+          <div class="col-4 col-lg">Description</div>
           <div class="col-lg-2 d-none d-lg-block">Location</div>
           <div class="col-lg d-none d-lg-block">Start date</div>
           <div class="col-lg d-none d-lg-block">End date</div>
@@ -317,9 +317,9 @@ $darkTheme = mysqli_fetch_array($darkTheme_result)[0];
           $passedEndTimeFormatted = date("h:i A", strtotime($passedEndDate));
           ?>
 
-        <div class="row mb-4">
+        <div class="row justify-content-around mb-4">
           <div class="col-4 col-lg"><a href="moreInfo.php?event_id=<?php echo $passedEvent_id ?>&userEvents_id=<?php echo $passedUserEvents_id ?>&invitedEvent=false"><?php echo $passedTitle ?></a></div>
-          <div class="col-5 col-lg"><?php echo $passedDescription ?></div>
+          <div class="col-4 col-lg"><?php echo $passedDescription ?></div>
           <div class="col-lg-2 d-none d-lg-block"><?php echo $passedLocation ?></div>
           <div class="col-lg d-none d-lg-block"><?php echo $passedStartDateFormatted."<br>".$passedStartTimeFormatted ?></div>
           <div class="col-lg d-none d-lg-block"><?php echo $passedEndDateFormatted."<br>".$passedEndTimeFormatted ?></div>
