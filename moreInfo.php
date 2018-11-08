7d438b7be759220ad05cad19fdd37f30049714ad
Ephraim Becker <emb16@outlook.com>
2018-11-08 10:58:58 -0500

    The date now aligns correctly with the location.

--- moreInfo.php
+++ moreInfo.php
@@ -266,7 +266,7 @@ $darkTheme = mysqli_fetch_array($darkTheme_result)[0];
 					}
 					else if($darkTheme == 1){
 					?>
-					<div class="card mx-lg-0 mx-auto bg-dark" style="width: 18rem;">
+					<div class="card mx-lg-0 mx-auto bg-dark">
 					<?php
 					}
 						ini_set("allow_url_fopen", 1);
@@ -279,9 +279,8 @@ $darkTheme = mysqli_fetch_array($darkTheme_result)[0];
 						echo '<img class="card-img-top" src="https://maps.googleapis.com/maps/api/staticmap?markers=color:red|'.$mapLatitude.','.$mapLongitude.'&zoom=15&size=300x300&key=AIzaSyDK-fy6hmPp1VEu8bSeKJoXkqWWgUO0dEo">';
 						?>
 						<div class="card-body">
-          		<p class="text-center card-text"><i class="material-icons align-text-top">location_on</i> <?php echo '<a class="card-link" href="https://www.google.com/maps/search/?api=1&query='.urlencode($location).'">'.$location ?></a></p>
-          		<p class="text-center card-text"><i class="material-icons align-text-top">access_time</i> <?php echo $startDateFormatted ?> at <?php echo $startTimeFormatted ?></p>
-          		<p class="text-center card-text">to <?php echo $endDateFormatted ?> at <?php echo $endTimeFormatted ?></p>
+          		<p class="card-text"><i class="material-icons align-text-top">location_on</i> <?php echo '<a class="card-link" href="https://www.google.com/maps/search/?api=1&query='.urlencode($location).'">'.$location ?></a></p>
+          		<p class="card-text"><i class="material-icons align-text-top">access_time</i> <?php echo $startDateFormatted ?> at <?php echo $startTimeFormatted ?><br> to <?php echo $endDateFormatted ?> at <?php echo $endTimeFormatted ?></p>
 						</div>
 					</div>
 					</div>
